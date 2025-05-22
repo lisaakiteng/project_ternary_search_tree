cbf37b5eeb830d6713607fe89ae7fd55c6006b3b
Mirriam <mirriamdianah@gmail.com>
2025-05-22 23:53:47 +0200

    Implemented insert function

--- Tstree.py
+++ Tstree.py
@@ -2,3 +2,9 @@ class Tstree:
 
     def __init__(self):
         self._root = None
+
+    def insert(self, string):
+        if self._root is None:
+            self._root = Tstree(string)
+        else:
+            self._root._insert(string)
--- TstreeNode.py
+++ TstreeNode.py
@@ -1,5 +1,24 @@
 class TstreeNode:
 
-   def __init__(self, string):
+    def __init__(self, string):
         self._string = string
         self._lt, self._eq, self._gt = None, None, None
+
+        
+   
+    def _insert(self, string):
+        if string == self._string:
+            if self._eq is None:
+                self._eq = TstreeNode(string)
+            else:
+                self._eq._insert(string)
+        elif string < self._string:
+            if self._lt is None:
+                self._lt = TstreeNode(string)
+            else:
+                self._lt._insert(string)
+        elif string > self._string:
+            if self._gt is None:
+                self._gt = TstreeNode(string)
+            else:
+                self._gt._insert(string)
