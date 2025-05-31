a94ded7abab98f6371b8887b32664251bda8fc8b
Lisa Angel Akiteng <lisaakiteng4@gmail.com>
2025-05-31 22:40:29 +0200

    Clean up and reset code for fresh start

--- ternary_search_tree.py
+++ ternary_search_tree.py
@@ -2,26 +2,17 @@ class TstreeNode:
 
     def __init__(self, string):
         self._string = string
+        self.terminates = False
         self._lt, self._eq, self._gt = None, None, None
-
         
    
     def _insert(self, string):
-        if string == self._string:
-            if self._eq is None:
-                self._eq = TstreeNode(string)
-            else:
-                self._eq._insert(string)
-        elif string < self._string:
-            if self._lt is None:
-                self._lt = TstreeNode(string)
-            else:
-                self._lt._insert(string)
-        elif string > self._string:
-            if self._gt is None:
-                self._gt = TstreeNode(string)
-            else:
-                self._gt._insert(string)
+
+                
+                
+    def _search(self, string):
+
+
 
 
 class TernarySearchTree:
@@ -30,9 +21,5 @@ class TernarySearchTree:
         self._root = None
 
     def insert(self, string):
-        if self._root is None:
-            self._root = TstreeNode(string)
-        else:
-            self._root._insert(string)
-            
+           
             
