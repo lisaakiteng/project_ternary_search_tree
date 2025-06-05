5504457bb59c92898ad173e202267935d8a9e952
Lisa Angel Akiteng <lisaakiteng4@gmail.com>
2025-06-05 15:15:31 +0200

    Replace '==' with 'is' for None

--- ternary_search_tree.py
+++ ternary_search_tree.py
@@ -14,8 +14,8 @@ class TstreeNode:
         """
         self._string = string
         self._terminates = False  # True if character at this node marks the end of a string
-        self._lt, self._eq, self._gt = None, None, None # Left, Equal, and Greater child nodes
-   
+        self._lt, self._eq, self._gt = None, None, None  # Left, Equal, and Greater child nodes
+
     def _insert(self, string):
         """
         Insert a string into the Ternary Search
@@ -48,26 +48,26 @@ class TstreeNode:
                 self._lt._insert(string)
             else:
                 self._lt._insert(string)
-                
-    def _search(self, string, check_prefix = False):
+
+    def _search(self, string, check_prefix=False):
         """
-        Search for a string or prefix 
+        Search for a string or prefix
         :param string: string to search
         :param check_prefix: if True, return True if prefix found; else, require exact match
         :return: True if string or prefix found, False otherwise
         """
         if string == '':
             return string == ''
-        
+
         first_char = string[0]
         remaining_char = string[1:]
-        
+
         if first_char == self._string:
-            if self._eq == None and remaining_char == '':
+            if self._eq is None and remaining_char == '':
                 return True
-            elif self._eq == None and remaining_char != '':
+            elif self._eq is None and remaining_char != '':
                 return False
-            elif (not self._eq == None) and remaining_char == '':
+            elif self._eq is not None and remaining_char == '':
                 if self._terminates:
                     return True
                 if check_prefix:
@@ -83,24 +83,24 @@ class TstreeNode:
                     pass
                 return self._eq._search(remaining_char, check_prefix)
         elif first_char > self._string:
-            if self._gt == None:
+            if self._gt is None:
                 return False
             else:
                 return self._gt._search(string, check_prefix)
         elif first_char < self._string:
-            if self._lt == None:
+            if self._lt is None:
                 return False
             else:
                 return self._lt._search(string, check_prefix)
 
-    def __str__(self, tab_length = 2):
+    def __str__(self, tab_length=2):
         """
         String representation of the Ternary Search Tree.
         """
         string = ("     " + " " * tab_length) + "char: " + self._string + ", Terminates: " + str(self._terminates)
         if self._lt:
             string += "\n_lt:" + self._lt.__str__(tab_length + 2)
-        if self._eq:    
+        if self._eq:
             string += "\n_eq:" + self._eq.__str__(tab_length + 2)
         if self._gt:
             string += "\n_gt:" + self._gt.__str__(tab_length + 2)
@@ -133,7 +133,7 @@ class TstreeNode:
             length += self._eq.__len__()
         if self._gt:
             length += self._gt.__len__()
-        return length      
+        return length
 
 
 class TernarySearchTree:
@@ -143,8 +143,8 @@ class TernarySearchTree:
 
     def insert(self, string):
         if string == '':
-            if self._root is None:  
-            # Create a root node with empty string and mark it terminates here
+            if self._root is None:
+                # Create a root node with empty string and mark it terminates here
                 self._root = TstreeNode('')
                 self._root._terminates = True
             elif self._root._string == '':
@@ -158,25 +158,25 @@ class TernarySearchTree:
                 new_root._terminates = True
                 self._root = new_root
                 return
-        if self._root == None:
+        if self._root is None:
             # print(f"The tree is empty. We create the root node with the first letter '{string[0]}'.\n")
             self._root = TstreeNode(string[0])
         self._root._insert(string)
-        
+
     def search(self, string, exact=False):
         if string == '':
             # Return True if searching prefix of empty string (empty prefix is always a prefix)
             return True if not exact else (
-                self._root is not None and self._root._string == '' and self._root._terminates
+                    self._root is not None and self._root._string == '' and self._root._terminates
             )
         return self._root._search(string, check_prefix=not exact)
-    
+
     def __str__(self):
-        if self._root == None:
+        if self._root is None:
             return ""
-        string = "terminates: "+str(self._root._terminates) + "\n"
+        string = "terminates: " + str(self._root._terminates) + "\n"
         if self._root._string == '':
-            if self._root._gt == None:
+            if self._root._gt is None:
                 return string
             else:
                 return string + self._root._gt.__str__()
@@ -184,7 +184,7 @@ class TernarySearchTree:
 
     def __repr__(self):
         return self.__str__()
-    
+
     def __len__(self):
         if self._root is None:
             return 0
@@ -195,14 +195,13 @@ class TernarySearchTree:
                 return 1 + len(self._root._gt)
         else:
             return len(self._root)
-        
-        
+
     def all_strings(self):
         if self._root is None:
-            return[]
+            return []
         if self._root._string == '':
-            if self._root._gt == None:
+            if self._root._gt is None:
                 return ['']
             else:
-                return[''] + self._root._gt.all_strings()
+                return [''] + self._root._gt.all_strings()
         return self._root.all_strings()
